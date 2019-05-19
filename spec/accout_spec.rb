b91ca0474e4b6669b886a556c66f2e4da309719b
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-19 13:31:46 +0100

    :sparkles: User can withdraw an ammount from account balance

--- spec/accout_spec.rb
+++ spec/accout_spec.rb
@@ -1,12 +1,18 @@
 require 'account'
 
 describe Account do
+  subject(:account) { described_class.new }
   describe '#deposit' do
     it { is_expected.to respond_to(:deposit) }
 
     it 'can deposit and ammount into the account balance' do
-      account = Account.new
       expect { account.deposit(100) }.to change { account.balance }.by(100)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'can withdraw and ammount into the account balance' do
+      expect { account.withdraw(100) }.to change { account.balance }.by(-100)
     end
   end
 end
