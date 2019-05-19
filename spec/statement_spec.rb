5826ea146c48df1c9b6e861b191d079bb2ae0e98
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-19 14:09:18 +0100

    :sparkles: Passed test to print current date to statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,18 +1,18 @@
 require 'statement'
 
 describe Statement do
-
+subject(:statement) { described_class.new}
   describe '#print_title' do 
     it 'Can print bank statement title' do
-      expect { subject.print_title }.to output('date || credit || debit || balance').to_stdout
+      expect { statement.print_title }.to output('date || credit || debit || balance').to_stdout
     end
   end
 
   describe '#date' do
-    xit 'Can print the current date' do
+    it 'Can print the current date' do
       @date = Time.now
       allow(Time).to receive(:now).and_return(@date)
-      expect { subject.date }.to output(@date.strftime('%Y-%m-%d')).to_stdout
+      expect { statement.date }.to output(@date.strftime('%Y-%m-%d')).to_stdout
     end
   end
 end
