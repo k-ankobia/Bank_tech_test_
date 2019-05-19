e1381b6e5d9251ab1721270b42a0dfef67fc307f
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-19 13:27:47 +0100

    :sparkles: User can deposit and amount into their account

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,11 @@
 class Account
+  attr_reader :balance
+  DEFAULT_BALANCE = 3000
   def initialize
+    @balance = DEFAULT_BALANCE
   end
 
-  def deposit
+  def deposit(amount)
+    @balance += amount
   end
 end
--- spec/accout_spec.rb
+++ spec/accout_spec.rb
@@ -3,5 +3,10 @@ require 'account'
 describe Account do
   describe '#deposit' do
     it { is_expected.to respond_to(:deposit) }
+
+    it 'can deposit and ammount into the account balance' do
+      account = Account.new
+      expect { account.deposit(100) }.to change { account.balance }.by(100)
+    end
   end
 end
